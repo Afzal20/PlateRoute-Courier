0e88bacdf6e0d152ff079ef8773df8f2f696e52e
Afzal20 <afzalhossen2019@gmail.com>
2026-08-30 22:21:55 +0600

    feat(settings): implement settings part 37

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -46,3 +46,4 @@ class SettingsScreen extends StatelessWidget {
           // Implement settings part 34
           // Implement settings part 35
           // Implement settings part 36
+          // Implement settings part 37
